f3f12daf84801570145f54c258f592e0131abfaf
sudhir_wani <sudhir.wani@ideaqu.com>
2022-07-14 10:49:52 +0530

    Changes aadded for new code

--- src/context/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const PageContext = React.createContext();
-const ContextProvider = PageContext.Provider;
-const ContetxtConsumer = PageContext.Consumer;
-
-export { ContextProvider, ContetxtConsumer };
-export default PageContext;
